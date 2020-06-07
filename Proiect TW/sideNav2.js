9af353f9bb3572c3c34ed2452099d6c4b863bb62
CristinaCiobanu710 <59660068+CristinaCiobanu710@users.noreply.github.com>
2020-06-07 14:33:19 +0300

    Add files via upload

---
+++ Proiect TW/sideNav2.js
@@ -0,0 +1,7 @@
+function openNav1() {
+  document.getElementById("mySidenav1").style.width = "200px";
+}
+
+function closeNav1() {
+   document.getElementById("mySidenav1").style.width = "0px";
+}
---
+++ Proiect TW/sideNav3.js
@@ -0,0 +1,7 @@
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "200px";
+}
+
+function closeNav2() {
+   document.getElementById("mySidenav2").style.width = "0px";
+}
