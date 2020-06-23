a49c2004bf2de9522d581039d18a3755c7b2f59e
codrin-postu <codrinpostu@outlook.com>
2020-06-23 07:54:04 +0300

    Added Statistics pages

---
+++ Proiect TW/db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$name = "APAX";
+$conn = mysqli_connect($servername, $username, $password, $name);
+if(!$conn){
+    die("Connection failed: ".mysql_connect_error());
+}
