518c9d3813c9e9071b4090e681ded40c16299ed8
teodorpaduraru <62095233+teodorpaduraru@users.noreply.github.com>
2020-06-23 08:51:36 +0300

    Add files via upload

---
+++ Proiect TW/desprenoi.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Despre noi</title>
+        <link rel="stylesheet" href="desprestil.css">
+    </head>
+    
+    <body>        
+        <div class="echipa">
+            <h1>Echipa noastră</h1>
+            <span class="border"></span>
+            <div class="poze">
+                <a href="#cristina"><img src="imagini/cristinamihaela.jpg"></a>
+                <a href="#codrin"><img src="imagini/codrinpostu.jpg"></a>
+                <a href="#teodor"><img src="imagini/teodorpaduraru.jpg"></a>
+            </div>
+            <div class="section" id="cristina">
+                <span class="name">Cristina Mihaela</span>
+                <span class="border"></span>
+                <p>Coechipiera echipei, Cristina este din Huși. Aceasta este în prezent studentă a Facultății de Informatică din Iași. </p>
+            </div>
+            <div class="section" id="codrin">
+                <span class="name">Codrin Postul</span>
+                <span class="border"></span>
+                <p>Coechipierul proiectului Codrin este din Iași. Acesta studiază în momentul actual la Facultatea de Informatică din Iași. </p>
+            </div>
+            <div class="section" id="teodor">
+                <span class="name">Teodor Paduraru</span>
+                <span class="border"></span>
+                <p>Coechipierul proiectului Teodor este din Iași. Acesta urmează studiile în prezent la Facultatea de Informatică din Iași. </p>
+            </div>
+
+            </div>
+        
+        <footer>
+            <p> © Cristina Mihaela, Postu Codrin, Paduraru Teodor 2020 </p>
+        </footer>   
+    </body>
+</html>
